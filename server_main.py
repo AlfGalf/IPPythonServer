5ce6edd8101a3fd5c30b2db5b7c9086dd7b63167
Alfie Richards <alfierchrds@gmail.com>
2021-02-03 13:26:45 +0000

    Initial

---
+++ server_main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+PORT = 8080
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello world'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
