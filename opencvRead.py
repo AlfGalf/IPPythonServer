e0b5e6b7af4d7205c7633ff1ee108225e065ca26
ArnauArmag <60605841+ArnauArmag@users.noreply.github.com>
2021-02-11 11:38:15 +0000

    Add files via upload
    OpenCV Testing

---
+++ opencvRead.py
@@ -0,0 +1,34 @@
+import cv2 as cv
+
+'''
+img=cv.imread('snake.jpeg')
+
+cv.imshow('Snake',img)
+'''
+def videoRecording():
+    capture=cv.VideoCapture(0)
+    counter=0
+    frames=[]
+    while True:
+        counter=counter+1
+        isTrue, frame=capture.read()
+        print(counter)
+        #cv.imshow('Video',frame)
+        print(frame.shape)
+        #if cv.waitKey(20) & 0xFF==ord('d'):
+        frames.append(frame)
+        if cv.waitKey(20) & counter>2:
+            break
+        
+    return frames
+
+def showFrames(frames):
+    for i in frames:
+        cv.imshow('Img',i)
+        cv.waitKey(0)
+    #capture.release()
+    cv.destroyAllWindows()
+
+#Example
+newRecording=videoRecording()
+showFrames(newRecording)
