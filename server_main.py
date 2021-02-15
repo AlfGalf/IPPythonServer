d66611d62d78d0593ac2070ae9f66ec6998163e6
Alfie Richards <alfierchrds@gmail.com>
2021-02-15 17:03:41 +0000

    Add image api point

--- server_main.py
+++ server_main.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 PORT = 8080
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello world'
+
+@app.route('/api/recieve_image', methods=["POST"])
+def image_input():
+
+    files = request.files.to_dict(flat=False)
+    print(files)
+    for i, file in enumerate(files):
+        print(file)
+        files[file][0].save(f'image-{i}.jpg')
+
+    return jsonify(
+        {
+            'msg': 'success',
+            'result': 'a'
+        })
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
