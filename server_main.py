4d6d5850159b8b6ff874f6031ffd732b0132e3d3
Alfie Richards <alfierchrds@gmail.com>
2021-03-20 11:11:40 +0000

    Add correction end point

--- server_main.py
+++ server_main.py
@@ -2,13 +2,14 @@ from flask import Flask, request, jsonify
 from MLModel import process_image
 from PIL import Image
 import io
-
+import os
+import pathlib
 
 app = Flask(__name__)
 
+
 @app.route('/api/recieve_image', methods=["POST"])
 def image_input():
-
     file = request.files.get('image', '')
     img = Image.open(file.stream)
     byteIO = io.BytesIO()
@@ -22,9 +23,18 @@ def image_input():
         })
 
 
+@app.route('/api/submit_correction', methods=["POST"])
+def submit_correction():
+    file = request.files.get('image', '')
+    file.save(os.path.join(pathlib.Path(__file__).parent.absolute(), file.filename))
+
+    return jsonify({'msg':'success'})
+
+
 @app.route('/')
 def test():
     return "Hello World"
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
